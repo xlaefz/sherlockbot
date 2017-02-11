5af0df097258cbbfe329a5190e2758e586cfcb39
Zac Guo <zacharyg@usc.edu>
2017-02-11 15:51:05 -0800

    branching receive message

--- index.js
+++ index.js
@@ -21,20 +21,20 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-// handler receiving messages
-app.post('/webhook', function (req, res) {
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        }
-    }
-    res.sendStatus(200);
-});
+// // handler receiving messages
+// app.post('/webhook', function (req, res) {
+//     var events = req.body.entry[0].messaging;
+//     for (i = 0; i < events.length; i++) {
+//         var event = events[i];
+//         if (event.message && event.message.text) {
+//             sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+//         }
+//     }
+//     res.sendStatus(200);
+// });
 
 // generic function sending messages
-function sendMessage(recipientId, message) {
+function sendTextMessage(recipientId, message) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
@@ -53,6 +53,87 @@ function sendMessage(recipientId, message) {
 }
 
 
+/*
+ * Message Event
+ *
+ * This event is called when a message is sent to your page. The 'message' 
+ * object format can vary depending on the kind of message that was received.
+ * Read more at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
+ *
+ * For this example, we're going to echo any text that we get. If we get some 
+ * special keywords ('button', 'generic', 'receipt'), then we'll send back
+ * examples of those bubbles to illustrate the special message bubbles we've 
+ * created. If we receive a message with an attachment (image, video, audio), 
+ * then we'll simply confirm that we've received the attachment.
+ * 
+ */
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var isEcho = message.is_echo;
+  var messageId = message.mid;
+  var appId = message.app_id;
+  var metadata = message.metadata;
+
+  // You may get a text or attachment but not both
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+  var quickReply = message.quick_reply;
+
+  if (isEcho) {
+    // Just logging message echoes to console
+    console.log("Received echo for message %s and app %d with metadata %s",
+      messageId, appId, metadata);
+    return;
+  } else if (quickReply) {
+    var quickReplyPayload = quickReply.payload;
+    console.log("Quick reply for message %s with payload %s",
+      messageId, quickReplyPayload);
+
+    sendTextMessage(senderID, "Quick reply tapped");
+    return;
+  }
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches any special
+    // keywords and send back the corresponding example. Otherwise, just echo
+    // the text we received.
+    switch (messageText) {
+      case 'movie':
+        sendTextMessage(senderID, "fuck movies");
+        break;
+
+      case 'show':
+        sendTextMessage(senderID, "fuck TV");
+        break;
+
+      case 'book':
+        sendTextMessage(senderID, "fuck words");
+        break;
+
+      case 'music':
+        sendTextMessage(senderID, "music is aight");
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+
+
+/*
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
@@ -127,3 +208,4 @@ request({
           }
       });
 }
+*/
