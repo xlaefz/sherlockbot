db555ab234a907b1d32c5fba7a3c848d4dae3db3
Zac Guo <zacharyg@usc.edu>
2017-02-11 14:48:04 -0800

    attempt greeting

--- index.js
+++ index.js
@@ -53,3 +53,46 @@ function sendMessage(recipientId, message) {
 };
 
 
+request.post({
+    method: 'POST',
+    uri: 'https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAazp7yTD5ABAImoZB5Oni2AdF0pU3UdEf2Ax0x33H5GS7ge7EECV2l7SAniZBNrY24Anw1ACoWQfcbJXZC0MCqbtdO3Li8DVEgTYRtunp1uiLNOn1kecYowS4WXeFUJuJidK6m2U9QDMd1tkcsw0KosjzClF5oCSH5G4D8SwZDZD',
+    qs: {
+        setting_type: 'call_to_actions',
+        thread_state: 'new_thread',
+            call_to_actions: [{
+                payload: 'GET_START'
+            }]
+        },
+    json: true
+}, (err, res, body) => {
+    // Deal with the response
+});
+
+request({
+    method: 'POST',
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+        access_token: ACCESS_TOKEN
+    },
+    json: {
+        recipient: {
+            id: SENDER_ID
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "generic",
+                    elements: {
+                        "title": "Your Title",
+                        "subtitle": "Welcome to my messenger bot",
+                        "image_url": "https://mybot.example.com/images/logo.jpg"
+                    }
+                }
+            }
+        }
+    }
+}, (err, res, body) => {
+    // Deal with the response
+});
+
