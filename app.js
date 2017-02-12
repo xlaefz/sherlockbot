2db625c75b2bfb38a4098f2b6b1c05063b47b712
Zac Guo <zacharyg@usc.edu>
2017-02-11 18:12:04 -0800

    reert const

--- app.js
+++ app.js
@@ -31,24 +31,25 @@ app.use(express.static('public'));
  */
 
 // App Secret can be retrieved from the App Dashboard
-var APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
   process.env.MESSENGER_APP_SECRET :
   config.get('appSecret');
 
 // Arbitrary value used to validate a webhook
-var VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
   (process.env.MESSENGER_VALIDATION_TOKEN) :
   config.get('validationToken');
 
 // Generate a page access token for your page from the App Dashboard
-var PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
   (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
   config.get('pageAccessToken');
 
 // URL where the app is running (include protocol). Used to point to scripts and 
 // assets located at this address. 
-var SERVER_URL = (process.env.SERVER_URL) ?
+const SERVER_URL = (process.env.SERVER_URL) ?
   (process.env.SERVER_URL) :
+  console.log(SERVER_URL);
   config.get('serverURL');
 
 if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
